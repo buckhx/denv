a40762c97512c87cb322daabe98e8f86abe08ff6
buckhx <buckhx@gmail.com>
2015-08-14 19:34:16 +0000

    init local_test

--- api/local_test.go
+++ api/local_test.go
@@ -27,4 +27,61 @@ func TestActivate(t *testing.T) {
 			t.Errorf("Activate(%q) wanted %s, got %s", c.in, c.want.ToString(), got.ToString())
 		}
 	}
+	denv, err := Activate("test")
+	if err != nil {
+		t.Errorf("Error creating Denv, %s", err)
+	}
+	if Info.Current != denv {
+		t.Errorf("Activate(test) did not properly assign Info.Current, %s", Info.ToString())
+	}
+}
+
+func TestDeactivate(t *testing.T) {
+	NewDenv("test") // Forces creation if not there
+	active, err := Activate("test")
+	if err != nil {
+		t.Errorf("Could not Activate(test), %s", err)
+	}
+	deactive := Deactivate()
+	if active != deactive {
+		t.Errorf("Deactivate() reaturned different denv, %p, %p", &active, &deactive)
+	}
+	if Info.Current != nil {
+		t.Errorf("Deactivate() did not clear Info.Current, %s", Info.ToString())
+	}
+}
+
+func TestList(t *testing.T) {
+	// Maybe implement a set
+	denvs := map[*Denv]bool {
+		NewDenv("d1"): true,
+		NewDenv("d2"): true,
+	}
+	ls := List()
+	set := make(map[string]int)
+	for d, _ := range ls {
+		set[d.Path] += 1
+	}	
+	for d, _ := range denvs {
+		if count, found := set[d.Path]; !found {
+			t.Errorf("Denvs not subset of List()\nDenvs %s\nList() %s", denvs, ls)
+		} else if count < 1 {
+			t.Errorf("Denvs not subset of List()\nDenvs %s\nList() %s", denvs, ls)
+		} else {
+			set[d.Path] = count - 1
+		}
+	}
+}
+
+func TestWhich(t *testing.T) {
+	NewDenv("test")
+	d, _ := Activate("test")
+	if Which() != d {
+		t.Errorf("Which() did not return active denv, %s, %s", d, Which())
+	}
+	d = Deactivate()
+	if Which() != nil {
+		t.Errorf("Which() did not return nil on deactivate, %s, %s", d, Which())
+	}
+
 }
