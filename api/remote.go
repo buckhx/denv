4151609fd10eca090aa7202ada53e67c836056a9
buckhx <buckhx@gmail.com>
2015-08-19 21:03:15 +0000

    settings up remote interface

--- api/remote.go
+++ api/remote.go
@@ -1,11 +1,44 @@
 package api
 
-import ()
+import (
+	"archive/tar"
+	"bytes"
+)
 
-func Pull(remote string) []string {
-	return []string{"Pulled"}
+func Pull(remote string) string {
+	pkg := pull(remote)
+	pkg = decrypt(pkg, passphrase)
+	thaw(pkg)
+	return pkg.dirs()
 }
 
-func Push() []string {
-	return []string{"Pushed"}
+func Push() string {
+	pkg := freeze()
+	pkg = encrypt(pkg, passphrase)
+	push(pkg, remote)
+	return remote
+}
+
+func freeze() string {
+	//only take denvs
+}
+
+func thaw() {
+	//return thawed denvs
+}
+
+func compress(paths []string) path string {
+
+}
+
+func decompress(path string) paths []string{
+
+}
+
+func encrypt(pkg string, passphrase string) string {
+
+}
+
+func decrypt(pkg string, passphrase string) string {
+
 }
