71ed96c67cea0248c19f7f7d10188f077188053f
buck <buckhx@gmail.com>
2015-09-11 01:18:45 +0200

    artifact -> denv_kern-arch

--- scripts/install.py
+++ scripts/install.py
@@ -22,11 +22,12 @@ print "Installing denv into {0}...".format(bindir)
 kernel, _, _, _, _, arch = platform.uname()
 if arch == 'x86_64':
     arch = 'amd64'
-dist = "_".join(["denv", kernel.lower(), arch.lower().replace("_", "")])
+dist = "{0}-{1}".format(kernel.lower(), arch.lower())
+artifact = "{0}_{1}".format("denv", dist)
 
 content = urllib.urlopen('https://api.github.com/repos/buckhx/denv/releases/latest').read()
 release = json.loads(content)
-link = [asset['browser_download_url'] for asset in release['assets'] if asset['name'] == dist][0]
+link = [asset['browser_download_url'] for asset in release['assets'] if asset['name'] == artifact][0]
 print "Downloading binary from " + link
 urllib.urlretrieve(link, bindir)
 os.chmod(bindir, 0755)
